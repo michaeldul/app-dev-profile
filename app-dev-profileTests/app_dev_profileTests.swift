9ca960b61331df1ffb68e032b1eaae9b6d7dbc77
Michael Dul <michaldul@michaels-mbp.home>
2018-01-31 15:05:38 +0000

    Initial Version

---
+++ app-dev-profileTests/app_dev_profileTests.swift
@@ -0,0 +1,36 @@
+//
+//  app_dev_profileTests.swift
+//  app-dev-profileTests
+//
+//  Created by Michael Dul on 31/01/2018.
+//  Copyright © 2018 MDC. All rights reserved.
+//
+
+import XCTest
+@testable import app_dev_profile
+
+class app_dev_profileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
